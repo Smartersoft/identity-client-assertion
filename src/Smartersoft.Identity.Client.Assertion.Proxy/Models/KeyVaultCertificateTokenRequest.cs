9b0759d36329ed28d1ff2affc1c2972805095e10
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2022-03-04 14:11:17 +0100

    docs: Documented all the proxy classes

--- src/Smartersoft.Identity.Client.Assertion.Proxy/Models/KeyVaultCertificateTokenRequest.cs
+++ src/Smartersoft.Identity.Client.Assertion.Proxy/Models/KeyVaultCertificateTokenRequest.cs
@@ -2,14 +2,30 @@
 
 namespace Smartersoft.Identity.Client.Assertion.Proxy.Models
 {
+    /// <summary>
+    /// Request an access token with a certificate in a KeyVault
+    /// </summary>
     public class KeyVaultCertificateTokenRequest : TokenRequest
     {
-        public Uri KeyVaultUri { get; set; }
-        public string CertificateName { get; set; }
+        /// <summary>
+        /// The Uri of your KeyVault hosting the certificate
+        /// </summary>
+        public Uri? KeyVaultUri { get; set; }
+
+        /// <summary>
+        /// Name of the certificate you wish to use
+        /// </summary>
+        public string? CertificateName { get; set; }
     }
 
+    /// <summary>
+    /// KeyVaultCertificateTokenRequestValidator
+    /// </summary>
     public class KeyVaultCertificateTokenRequestValidator : AbstractValidator<KeyVaultCertificateTokenRequest>
     {
+        /// <summary>
+        /// KeyVaultCertificateTokenRequestValidator constuctor
+        /// </summary>
         public KeyVaultCertificateTokenRequestValidator()
         {
             Include(new TokenRequestValidator());
@@ -18,7 +34,7 @@ namespace Smartersoft.Identity.Client.Assertion.Proxy.Models
                 .NotEqual("string");
             RuleFor(r => r.KeyVaultUri)
                 .NotEmpty()
-                .Must(v => v.IsAbsoluteUri)
+                .Must(v => v?.IsAbsoluteUri == true)
                 .WithMessage("Only absolute Uris permitted");
         }
     }
--- src/Smartersoft.Identity.Client.Assertion.Proxy/Models/KeyVaultKeyTokenRequest.cs
+++ src/Smartersoft.Identity.Client.Assertion.Proxy/Models/KeyVaultKeyTokenRequest.cs
@@ -2,20 +2,36 @@
 
 namespace Smartersoft.Identity.Client.Assertion.Proxy.Models
 {
+    /// <summary>
+    /// Request an access token with a key in a KeyVault
+    /// </summary>
     public class KeyVaultKeyTokenRequest : TokenRequest
     {
-        public Uri KeyUri { get; set; }
-        public string KeyThumbprint { get; set; }
+        /// <summary>
+        /// The Uri of the Key to use, found in KeyId in the KeyVault
+        /// </summary>
+        public Uri? KeyUri { get; set; }
+
+        /// <summary>
+        /// Base64encoded hash of the certificate, use the proxy /api/token/kv-key-info endpoint to find
+        /// </summary>
+        public string? KeyThumbprint { get; set; }
     }
 
+    /// <summary>
+    /// KeyVaultKeyTokenRequestValidator
+    /// </summary>
     public class KeyVaultKeyTokenRequestValidator : AbstractValidator<KeyVaultKeyTokenRequest>
     {
+        /// <summary>
+        /// KeyVaultKeyTokenRequestValidator constructor
+        /// </summary>
         public KeyVaultKeyTokenRequestValidator()
         {
             Include(new TokenRequestValidator());
             RuleFor(r => r.KeyUri)
                 .NotEmpty()
-                .Must(v => v.IsAbsoluteUri)
+                .Must(v => v?.IsAbsoluteUri == true)
                 .WithMessage("Only absolute Uris permitted");
             RuleFor(r => r.KeyThumbprint)
                 .NotEmpty()
